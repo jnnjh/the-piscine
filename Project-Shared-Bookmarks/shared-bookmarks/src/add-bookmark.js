3e066b4b14410c4314de062d372ebfa3fb0a4895
jnnjh <joannejhoieacar@gmail.com>
2026-02-21 14:25:13 +0100

    added a temporary user to save bookmark data

--- Project-Shared-Bookmarks/shared-bookmarks/src/add-bookmark.js
+++ Project-Shared-Bookmarks/shared-bookmarks/src/add-bookmark.js
@@ -1,16 +1,27 @@
 import { getUserIds } from "./storage.js";
-import { setData } from "./storage.js";
+import { getData, setData } from "./storage.js";
+
+const currentUser = "1"; //this one can be changed based on dropdown later on...
 const addBookmarkButton = document.getElementById("add-bookmark");
+let bookmarks = getData(currentUser) || [];
 
 function add() {
     const addTitle = document.getElementById("bookmark-title");
     const addDescription = document.getElementById("bookmark-description");
     const addLink = document.getElementById("bookmark-link");  
 
-    const title = addTitle.value;
-    const description = addDescription.value;
-    const link = addLink.value; 
+    const titleVal = addTitle.value;
+    const descriptionVal = addDescription.value;
+    const linkVal = addLink.value; 
 
-    const user = "1"; //this one can be changed based on dropdown later on...
-    user['user']
+    const newBookmark = [
+        {
+            title: titleVal,
+            description: descriptionVal,
+            link: linkVal
+        }
+    ]
+
+    bookmarks.push(newBookmark);
 }
+
