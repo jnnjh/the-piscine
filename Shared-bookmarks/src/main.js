6fc959ef113637754e48344ebe8402b26d372922
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2026-02-22 21:44:07 +0100

    updated main.js file

--- Shared-bookmarks/src/main.js
+++ Shared-bookmarks/src/main.js
@@ -1,6 +1,3 @@
 import { setupUserSelection } from './user-selection.js';
-import { initBookmarkForm } from './add-bookmark.js';
 //initialize user-selection dropdown
 setupUserSelection();
-//initialize bookmark-form 
-initBookmarkForm();
