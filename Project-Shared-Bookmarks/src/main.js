fe752eaf8b8273c8abd3f70f1d124f3e920c2156
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2026-03-03 21:25:59 +0100

    Update index.html and main.js

--- Project-Shared-Bookmarks/src/main.js
+++ Project-Shared-Bookmarks/src/main.js
@@ -1,7 +1,8 @@
 import { initUserSelection } from "./user-selection.js";
+import { initAddBookmarkForm } from "./add-bookmark.js";
+import { renderBookmarks } from "./display.js";
 
 document.getElementById("app").innerHTML = "";
 
-initUserSelection(() => {
-    console.log("User selection initialized");
-});
+initUserSelection(renderBookmarks);
+initAddBookmarkForm(renderBookmarks);
