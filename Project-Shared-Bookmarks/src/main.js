303046d376de0da3d7f5a031f8f4752f2f16c0af
Anastasiia Hlushkova <glushkova.a.y@gmail.com>
2026-03-05 22:42:17 +0100

    Connect style.css via Vite import

--- Project-Shared-Bookmarks/src/main.js
+++ Project-Shared-Bookmarks/src/main.js
@@ -1,3 +1,4 @@
+import "./style.css";
 import { initUserSelection } from "./user-selection.js";
 import { initAddBookmarkForm } from "./add-bookmark.js";
 import { renderBookmarks } from "./display.js";
