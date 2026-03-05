e107219051ecaaa17bc35ad2ed7db512a5ab1f80
jnnjh <joannejhoieacar@gmail.com>
2026-03-05 17:05:02 +0100

    deleted unecessary files for merging

--- Project-Shared-Bookmarks/src/main.js
+++
@@ -1,8 +0,0 @@
-import { initUserSelection } from "./user-selection.js";
-import { initAddBookmarkForm } from "./add-bookmark.js";
-import { renderBookmarks } from "./display.js";
-
-document.getElementById("app").innerHTML = "";
-
-initUserSelection(renderBookmarks);
-initAddBookmarkForm(renderBookmarks);
