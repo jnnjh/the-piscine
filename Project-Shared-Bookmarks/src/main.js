0f5335cc44539fb0e08a6c4457a08d793bac8ab1
jnnjh <joannejhoieacar@gmail.com>
2026-03-06 02:48:14 +0100

    fixed non-closing modal.

--- Project-Shared-Bookmarks/src/main.js
+++ Project-Shared-Bookmarks/src/main.js
@@ -7,3 +7,17 @@ document.getElementById("app").innerHTML = "";
 
 initUserSelection(renderBookmarks);
 initAddBookmarkForm(renderBookmarks);
+
+
+//modal logic here
+const modal = document.getElementById("modal");
+const openBtn = document.getElementById("openModalBtn");
+const closeBtn = document.getElementById("closeModalBtn");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("show");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("show");
+});
